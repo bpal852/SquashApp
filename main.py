051dd91f56f95c993648fc470a15d2c89b0a0d09
Drafterthoughts <draftfpl0@gmail.com>
2024-02-23 16:18:23 +0800

    Wednesday update

--- main.py
+++ main.py
@@ -689,7 +689,7 @@ def scrape_ranking_page(league_id, year):
 
 
 # Change dictionary if you want specific week
-for div in tuesday.keys():
+for div in wednesday.keys():
     league_id = f"D00{divisions[div]}"
 
     # Scrape Team Summary page
