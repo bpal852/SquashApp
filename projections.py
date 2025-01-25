b25d54689b1b8784c04239b5b61c4dce1205d1d9
Drafterthoughts <draftfpl0@gmail.com>
2025-01-25 16:36:25 +0800

    projections update

--- projections.py
+++ projections.py
@@ -8,9 +8,9 @@ from collections import Counter, defaultdict
 
 # Set parameters
 year = "2024-2025"
-num_simulations = 1999
+num_simulations = 4999
 home_advantage_factor = 0.05
-unpredictability_factor = 0.2
+unpredictability_factor = 0.1
 
 # Define functions
 
